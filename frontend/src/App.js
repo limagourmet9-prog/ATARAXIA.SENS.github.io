20dbfbddf074706b3fbbc0c3b2dabc8303288f36
Brian Marroquín Ambriz <limagourmet9@gmail.com>
2026-09-18 19:11:08 -0600

    Make router base path environment-aware

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2131,7 +2131,7 @@ const Home = () => {
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL === '.' ? '' : (process.env.PUBLIC_URL || '')}>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
